e24036b1c764e609fbd98bda0750e100d7e55930
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2025-02-28 15:07:53 +0700

    update login BE

---
+++ resources/views/backend/auth/login.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> FIVE cosmetics | Login  </title>
+
+    <link href=" backend/css/bootstrap.min.css" rel="stylesheet">
+    <link href=" backend/css/font-awesome.css" rel="stylesheet">
+
+    <link href="  backend/css/animate.css" rel="stylesheet">
+    <link href="backend/css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+<div class="loginColumns animated fadeInDown">
+    <div class="row">
+
+        <div class="col-md-6">
+            <h2 class="font-bold">Welcome to FIVE cosmetic</h2>
+
+            <p>
+                 Save The Best For You !
+            </p>
+
+            <p>
+                XTLTH cosmetics là cửa hàng mỹ phẩm chính hãng online cung cấp nhiều thương hiệu nổi tiếng như Vichy, Bioderma, Senka, Shiseido, Innisfree, La Roche-Posay, …
+            </p>
+
+            <p>
+                Cửa Hàng Chuyên Bán Mỹ Phẩm Chính Hãng Chọn Lọc Các Thương Hiệu Từ Anh, Pháp, Mỹ, Nhật.
+            </p>
+
+            <p>
+                <small> Địa chỉ : Hà Nội , Việt Nam  </small>
+            </p>
+
+        </div>
+        <div class="col-md-6">
+            <div class="ibox-content">
+                <form class="m-t" role="form" action="index.html">
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Username" required="">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" required="">
+                    </div>
+                    <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+                    <a href="#">
+                        <small>Forgot password?</small>
+                    </a>
+
+                    <p class="text-muted text-center">
+                        <small>Do not have an account?</small>
+                    </p>
+                    <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                </form>
+                <p class="m-t">
+                    <small>FIVE cosmetics  EST 2004 </small>
+                </p>
+            </div>
+        </div>
+    </div>
+    <hr/>
+    <div class="row">
+        <div class="col-md-6">
+            Copyright FIVE cosmetic
+        </div>
+        <div class="col-md-6 text-right">
+            <small>© 2004-2025</small>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Backend\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login',  [AuthController::class, 'login'])->name('login');
+
