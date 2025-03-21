840e2ccf2c4c51dc2afc7f87822fc9fdd3bcc767
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2025-03-22 00:51:27 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -20,29 +20,29 @@ Route::get('/', function () {
 //BACKEND
 Route::get('admin',  [AuthController::class, 'index'])->name('auth.admin');//->middleware(LoginMiddleware::Class);
 Route::post('login',  [AuthController::class, 'login'])->name('auth.login');
-Route::get('dashboard/index', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(AuthMiddleware::class);
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(AuthMiddleware::class);
 Route::get('logout', [AuthController:: class, 'logout'])->name('auth.logout');
 
 
 // USER
-Route::get('user/index', [Backend\UserController ::class, 'index'])->name('user.index')->middleware(AuthMiddleware::class);
-Route::get('usergroup/index', [UserGroupController ::class, 'index'])->name('usergroup.index')->middleware(AuthMiddleware::class);
+Route::get('user', [Backend\UserController ::class, 'index'])->name('user.index')->middleware(AuthMiddleware::class);
+Route::get('usergroup', [UserGroupController ::class, 'index'])->name('usergroup.index')->middleware(AuthMiddleware::class);
 // Film
-Route::get('films/index', [FilmController::class, 'index'])->name('films.index ')->middleware(AuthMiddleware::class);
+Route::get('films', [FilmController::class, 'index'])->name('films.index ')->middleware(AuthMiddleware::class);
 
  //movie showtimes
-Route::get('movieShowtime/index', [ Backend\movieShowtimeController::class, 'index'])->name('movieShowtime.index ')->middleware(AuthMiddleware::class);
+Route::get('movieShowtime', [ Backend\movieShowtimeController::class, 'index'])->name('movieShowtime.index ')->middleware(AuthMiddleware::class);
 
 
 // Comments
-Route::get('comments/index', [CommentsController::class, 'index'])->name('comments.index ')->middleware(AuthMiddleware::class);
+Route::get('comments', [CommentsController::class, 'index'])->name('comments.index ')->middleware(AuthMiddleware::class);
 
 
 // revenue
-Route::get('revenue/index', [Backend\RevenueController::class, 'index'])->name('revenue.index ')->middleware(AuthMiddleware::class);
+Route::get('revenue', [Backend\RevenueController::class, 'index'])->name('revenue.index ')->middleware(AuthMiddleware::class);
 
 //ticket
-Route::get('ticket/index', [Backend\ticketController::class, 'index'])->name('ticket.index ')->middleware(AuthMiddleware::class);
+Route::get('ticket', [Backend\ticketController::class, 'index'])->name('ticket.index ')->middleware(AuthMiddleware::class);
 
 
 // FE _home
