6f469553e2071623efab135b26dea3c8bf99d6f7
Hoàng Diệu Linh <136173788+dieulinh0809@users.noreply.github.com>
2025-03-26 01:08:17 +0700

    update model

--- app/Models/Ghe.php
+++ app/Models/Ghe.php
@@ -4,12 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ghe extends Model
 {
     //
     use HasFactory;
     protected $table = 'ghe';
-    protected $fillable = ['M_id', 'Ghe', 'TrangThai'];
+    protected $fillable = ['PC_id', 'idG', 'status'];
 
+    public function phongChieu(): BelongsTo {
+        return $this->belongsTo(PhongChieu::class, 'PC_id', 'PC_id');
+    }
+     public function ve(): HasMany {
+        return $this->HasMany(Ve::class, 'idG', 'idG');
+     }
+
+
+     //Ghe hasMany Ve: Một ghế có nhiều vé (cho các suất chiếu khác nhau).
+    //Ve belongsTo Ghe: Một vé thuộc về một ghế duy nhất.
+
+
+    /* One to one :  hasOne ><belongsto
+     *One to many :hasMany >< belongsto
+     * many to many : belongsToMany()
+     *
+     *
+     * belongsTo giúp bạn xác định "cha" của một model.
+     *  Model chứa khóa ngoại sẽ sử dụng belongsTo để trỏ đến model mà khóa ngoại đó tham chiếu đến.
+     *
+     *
+     *
+     * */
 }
--- app/Models/Ve.php
+++ app/Models/Ve.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ve extends Model
 {
-    //trong bảng vé có chứa pcid , mình truy xuất pcid trong lịch chiếu . Từ pcid sẽ in ra các ghế . lấy ghế đã đặt bằng id Ghế
+    //trong bảng vé có chứa pcid , mình truy xuất pcid trong lịch chiếu .
+    // Từ pcid sẽ in ra các ghế . lấy ghế đã đặt bằng id Ghế
 
 
 }
