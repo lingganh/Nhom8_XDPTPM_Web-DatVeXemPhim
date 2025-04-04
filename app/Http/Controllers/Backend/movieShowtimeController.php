f26718160b11c91a685686cac89e6df9f5baccd7
tra123456789 <145311985+tra123456789@users.noreply.github.com>
2025-04-04 18:22:21 +0700

    update

--- app/Http/Controllers/Backend/movieShowtimeController.php
+++ app/Http/Controllers/Backend/movieShowtimeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\Film;
+use Illuminate\Http\Request;
 class movieShowtimeController
 {
 
@@ -13,5 +15,33 @@ class movieShowtimeController
     public function index(){
 
         return view('backend.moviveShowtime.index');
+    }
+
+    public function movieIndex(Request $request){
+
+        $phims = Film::query();
+
+        if($request->has('keyword')){
+            $phims->where('tenPhim', 'like', '%'.$request->keyword.'%');
+        }
+        if($request->has('sort')){
+
+
+            if($request->sort == 'name_z_a'){
+                $phims->orderBy('tenPhim', 'desc');
+            }
+            if($request->sort == 'name_a_z'){
+                $phims->orderBy('tenPhim', 'asc');
+            }
+            if($request->sort == 'time_asc'){
+
+                $phims->orderBy('thoiLuong', 'asc');
+            }
+            if($request->sort == 'time_desc'){
+                $phims->orderBy('thoiLuong', 'desc');
+            }
+        }
+        $phims = $phims->paginate(12);
+        return view('backend.moviveShowtime.movie_index', compact('phims'));
     }
 }
