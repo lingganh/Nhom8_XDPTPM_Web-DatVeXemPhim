aab233224ab060a780af36de1fa85d22ff7c0170
Hoàng Diệu Linh <dieulinh120411@gmail.com>
2025-04-09 03:00:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::post('user', [Backend\UserController::class, 'create'])->name('user.creat
 Route::put('user/{user}', [Backend\UserController::class, 'update'])->name('user.update')->middleware(AuthMiddleware::class);
 Route::delete('user/{user}', [Backend\UserController::class, 'delete'])->name('user.destroy')->middleware(AuthMiddleware::class);
 // Film
-Route::get('films', [FilmController::class, 'index'])->name('films.index')->middleware(AuthMiddleware::class);
+Route::get('films', [FilmController::class, 'index'])->name('films.index ')->middleware(AuthMiddleware::class);
  Route::post('filmsupdate/{id}', [FilmController::class, 'update'])->middleware(AuthMiddleware::class);
  Route::post('filmscreate', [FilmController::class, 'create'])->name('films.create')->middleware(AuthMiddleware::class);
 Route::get('filmsdelete/{id}', [FilmController::class, 'delete'])->name('films.delete')->middleware(AuthMiddleware::class);
