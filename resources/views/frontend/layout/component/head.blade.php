bd557a79a4c12416ca94b0313be65742fc276928
Xuân Lê <lephuongxu3@gmail.com>
2025-04-10 01:04:37 +0700

    up CTP

--- resources/views/frontend/layout/component/head.blade.php
+++ resources/views/frontend/layout/component/head.blade.php
@@ -4,8 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Trang Chủ</title>
 
-    <link rel="stylesheet" href="frontend/assets/css/style-starter.css">
-    <link rel="stylesheet" href="frontend/style.css">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style-starter.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/style.css') }}">
     <link href="//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,600&display=swap"
           rel="stylesheet">
 
