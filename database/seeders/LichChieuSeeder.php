4f6d23d01d0c468e3945c53b28d6be5734b482b3
Xuân Lê <lephuongxu3@gmail.com>
2025-04-17 22:52:34 +0700

    up csdlLC

--- database/seeders/LichChieuSeeder.php
+++ database/seeders/LichChieuSeeder.php
@@ -12,6 +12,14 @@ class LichChieuSeeder extends Seeder
 {
     public function run()
     {
+        DB::table('lich_chieu')->insert([
+            [ 'PC_id' => '1', 'M_id' => '1', 'ngayChieu' => '2025-04-13', 'gioBD' => '2025-04-13 18:00:00', 'thoiLuong' => 120],
+            [ 'PC_id' => '2', 'M_id' => '1', 'ngayChieu' => '2025-04-13', 'gioBD' => '2025-04-13 8:00:00', 'thoiLuong' => 120],
+            [ 'PC_id' => '1', 'M_id' => '1', 'ngayChieu' => '2025-04-13', 'gioBD' => '2025-04-13 20:00:00', 'thoiLuong' => 120],
+            [ 'PC_id' => '1', 'M_id' => '1', 'ngayChieu' => '2025-04-14', 'gioBD' => '2025-04-14 8:00:00', 'thoiLuong' => 120],
+            [ 'PC_id' => '1', 'M_id' => '1', 'ngayChieu' => '2025-04-14', 'gioBD' => '2025-04-14 18:00:00', 'thoiLuong' => 120],
+            [ 'PC_id' => '2', 'M_id' => '2', 'ngayChieu' => '2025-01-06', 'gioBD' => '2025-01-06 20:00:00', 'thoiLuong' => 150],
+        ]);
         // Lấy ngày hôm nay
         $today = Carbon::today();
 
