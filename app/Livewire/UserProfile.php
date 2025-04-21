97048e38c7b3b6f720d9a1e7fee42097ddfa41bf
Hoàng Diệu Linh <dieulinh120411@gmail.com>
2025-04-21 15:28:52 +0700

    update user profile

--- app/Livewire/UserProfile.php
+++ app/Livewire/UserProfile.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use App\Models\Ve;
 use Livewire\Component;
 
 class UserProfile extends Component
@@ -10,7 +11,9 @@ class UserProfile extends Component
     public function render()
     {
         $user = Auth()->user();
+         $ves = Ve::where('user_id',  $user->id)->orderByDesc('created_at')->paginate(10);
 
-        return view('livewire.user-profile' ,compact('user'))->extends('layouts.app') ->section('content');
+
+        return view('livewire.user-profile' ,compact('user' ,'ves'))->extends('layouts.app') ->section('content');
     }
 }
